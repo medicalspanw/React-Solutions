c39de33c53ae54b072c305a9c065ede167ed2d75
medicalspanw <medicalspanw@gmail.com>
2019-09-17 13:21:43 -0700

    Update Star.js

--- Star.js
+++ Star.js
@@ -17,28 +17,6 @@ class Star extends React.Component {
     }
 }
 export default Star;
-/*
-            <svg height="14" width="14">
-            <polygon points="  7.0,   0.35 
-                               2.8,  13.51
-                              13.3,   5.11 
-                               0.7,   5.11 
-                              11.2,  13.51"
-                             style={{fill: 'gold', fillRule: 'nonzero'}} />
-            </svg>
-
-            <svg height="200" width="200" style={{backgroundColor: 'white'}}>
-            <polygon points="100,   5 
-                              40, 193 
-                             190,  73 
-                              10,  73 
-                             160, 193"
-                             style={{fill: 'gold', fillRule: 'nonzero'}} />
-            </svg>
-            <svg height="210" width="210" style={{backgroundColor: 'white'}}>
-                <polygon points="100,10 40,198 190,78 10,78 160,198"style={{fill: 'lime', stroke: 'purple', strokeWidth:5, fillRule: 'nonzero'}} />
-            </svg>
-*/
 
 
 
